f8aebe4fef2c20ae8f4da2d9b728f7b44d354e3f
LoftyLettuce <lubolulubo2@gmail.com>
2024-03-07 16:06:34 +0700

    Create grids

--- javascripts.js
+++ javascripts.js
@@ -13,16 +13,32 @@ inputBox.addEventListener("keydown", function(e){
       {
         console.log("stop");
         clearInterval(opacing);
-        createDivs();
         inputBox.remove();
         question.remove();
+        createDivs(Number(e.target.value));
       }
       opacity -= 0.3;
     }, 100)
   }
 });
-function createDivs(){
+function createDivs(n){
   let board = document.createElement('div');
   board.className = "board";
   body.appendChild(board);
+  console.log(typeof(board.offsetHeight));
+  for (let i = 0; i < n; i++)
+  {
+    let row = document.createElement('div');
+    row.className = "row";
+    row.style.height = board.offsetHeight/n + "vw";
+    for (let u = 0; u < n; u++)
+    {
+      let column = document.createElement('div');
+      column.className = "column";
+      column.style.height = "100%";
+      column.style.width = board.offsetWidth/n + "vw";
+      row.appendChild(column);
+    }
+    board.appendChild(row);
+  }
 }
