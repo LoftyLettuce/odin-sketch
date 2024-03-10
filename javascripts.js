fb7268dbac97ff663cb2d348f9f12d262a4da490
LoftyLettuce <lubolulubo2@gmail.com>
2024-03-10 10:01:53 +0700

    Add mix color feature, changing random color feature

--- javascripts.js
+++ javascripts.js
@@ -45,23 +45,44 @@ function createDivs(n){
     }
   }
 }
+function darken(s, d, e, f){
+  let end1 = s.indexOf(',');
+  let end2 = s.slice(end1+1).indexOf(',')+end1;
+  let end3 = s.slice(end2+1).indexOf(')')+end2;
+  let a = Number(s.slice(4, end1));
+  let b = Number(s.slice(end1+1, end2+1));
+  let c = Number(s.slice(end2+2, end3+1));
+  return "rgb(" +(a+d)/2 +"," +(b+e)/2 +"," +(e+f)/2 +")";
+}
 function addColor(){
-  let a = Math.random()*255;
-  let b = Math.random()*255;
-  let c = Math.random()*255;
+  let a = Math.random()*254+1;
+  let b = Math.random()*254+1;
+  let c = Math.random()*254+1;
   return "rgb(" +a +"," +b +"," +c +")";
 }
 // draw function
-function changingColor(e){
-  if (e.target.className == "column")
+function changingColor(e, color, r, b, g){
+  if (e.target.className == "colored")
   {
-    e.target.style.backgroundColor = addColor();
+    e.target.style.backgroundColor = darken(e.target.style.backgroundColor, r, b, g);
+  }
+  else if (e.target.className == "column")
+  {
+    e.target.className = "colored";
+    e.target.style.backgroundColor = color;
   }
 }
 window.addEventListener('mousedown', function(e){
-  changingColor(e);
+  let color = addColor()
+  let end1 = color.indexOf(',');
+  let end2 = color.slice(end1+1).indexOf(',')+end1;
+  let end3 = color.slice(end2+1).indexOf(')')+end2;
+  let r = Number(color.slice(4, end1));
+  let b = Number(color.slice(end1+1, end2+1));
+  let g = Number(color.slice(end2+2, end3+1));
+  changingColor(e, color, r, b, g);
   this.window.addEventListener('mouseover', draw = function(e){
-    changingColor(e);
+    changingColor(e, color, r, b, g);
   })
 })
 window.addEventListener('mouseup', function(){
